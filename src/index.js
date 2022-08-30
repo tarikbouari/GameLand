20e62c55d98e8feeb711c1498e7ba7aadd77a160
Lena <summyalena@gmail.com>
2022-08-30 12:27:40 +0100

    added the footer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import "./css/main.css";
-import "./css/desktop.css"
+import './css/main.css';
+import './css/desktop.css';
+
+const myButton = document.getElementById('Btn');
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    myButton.style.display = 'block';
+  } else {
+    myButton.style.display = 'none';
+  }
+}
+
+window.onscroll = function() { scrollFunction(); };
+
+myButton.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
