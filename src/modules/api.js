7cf0637d98352c97f9d6f9a9fc20e6d548bbf23e
Rikstone <tarikbouari@gmail.com>
2022-08-30 11:13:56 -0700

    fetch Api data

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,11 +1,14 @@
+import { loadCard } from "./render.js";
+
 const key =
   "https://api.rawg.io/api/games?key=2525d548c34d45f18dd11454eed1df14";
 
-  const getGame = async (gameData) => {
+const getGame = async () => {
   const res = await fetch(key);
   const jsonData = await res.json();
-  gameData = jsonData.results;
-  console.log(gameData);
+  const gameData = jsonData.results;
+  console.log(gameData[0].id);
+  loadCard(gameData);
 };
 
 export { getGame, key };
