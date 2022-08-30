ffa45eadfc61d648e542db8edcddeca8cd91e924
Rikstone <tarikbouari@gmail.com>
2022-08-30 06:05:53 -0700

    update menu.js

---
+++ src/modules/menu.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById('menu');
+const navUl = document.getElementById('nav-ul');
+
+menuIcon.addEventListener('click', (e) => {
+  e.preventDefault();
+  navUl.classList.toggle('ul-toggle');
+});
