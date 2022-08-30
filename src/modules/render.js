fc2998c03b2f2aa76288a9085d5e9d8898a4cb84
Rikstone <tarikbouari@gmail.com>
2022-08-30 11:17:14 -0700

    update menu.js

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,12 +1,44 @@
-
-const container = document.getElementById('card-container');
+const container = document.getElementById("card-container");
 
 const loadCard = (games) => {
-   games.forEach( (game ) => {
+  games.forEach((game) => {
+    const div = document.createElement("div");
+    div.classList.add("box-card");
 
-     c
+    const divImg = document.createElement("div");
+    divImg.classList.add("img-box");
 
+    const image = document.createElement("img");
+    image.setAttribute("src", `${game.background_image}`);
 
-   } ) 
-} 
+    divImg.appendChild(image);
 
+    const box = document.createElement("div");
+    box.classList.add("box-likes");
+
+    const h3 = document.createElement("span");
+    h3.textContent = `${game.name}`;
+
+    const icon = document.createElement("i");
+    icon.classList.add("icon");
+
+    const span = document.createElement("span");
+    span.textContent = `${game.id} likes`;
+
+    box.append(h3, span, icon);
+
+    const comments = document.createElement("button");
+    comments.classList.add("btn-comment", "mx-1");
+    comments.textContent = "Comments";
+
+    const reservations = document.createElement("button");
+    reservations.classList.add("btn-reservation", "mx-1");
+    reservations.textContent = "Reservations";
+
+    div.append(divImg, box, comments, reservations);
+
+    container.appendChild(div);
+  });
+};
+
+export { loadCard, container };
