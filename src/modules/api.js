96fe240de9b4a1b4807279c9d336a303409959c9
Rikstone <tarikbouari@gmail.com>
2022-08-31 04:46:51 -0700

    create function getLikes

--- src/modules/api.js
+++ src/modules/api.js
@@ -7,8 +7,35 @@ const getGame = async () => {
   const res = await fetch(key);
   const jsonData = await res.json();
   const gameData = jsonData.results;
-  console.log(gameData[0].id);
   loadCard(gameData);
 };
 
-export { getGame, key };
+// new indetifier
+const id = "AFFSRiO8tq3BNoRoizLk";
+const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${id}/likes/`;
+
+//post likes
+const postLikes = async (array) => {
+  const res = await fetch(url, {
+    method: "Post",
+    body: JSON.stringify(array),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  const storage = await res.json();
+  return storage;
+};
+
+//get likes
+
+const getLikes = async () => {
+  const res = await fetch(url);
+  const storage = await res.json();
+  return storage;
+};
+
+
+export { getGame, postLikes, getLikes };
