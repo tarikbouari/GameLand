9b19e73ba08a42b4f346e550554cd379d4bd6241
Rikstone <tarikbouari@gmail.com>
2022-08-31 04:49:39 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./css/main.css";
 import "./css/desktop.css";
 import "./modules/menu.js";
-import { getGame, key } from "./modules/api.js";
+import { getGame, postLikes,getLikes } from "./modules/api.js";
 // import { loadCard } from "./modules/render.js";
 
  getGame();
-// loadCard();
+
+ getLikes()
 
 const myButton = document.getElementById("Btn");
 
