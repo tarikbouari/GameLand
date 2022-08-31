08518c2900213d78d852f45f610286eabd257c6c
Rikstone <tarikbouari@gmail.com>
2022-08-31 12:00:31 -0700

    add function countgame

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -2,5 +2,14 @@ import { getGame } from "./api.js";
 
 const gameCounter = document.getElementById('games');
 
+const countgame = async () => {
+    const data = await getGame();
+    const count = data.length;
+    console.log(count);
+    gameCounter.textContent = `Games(${count})`
+}
+countgame();
 
+
+export {countgame}; 
    
