db98735732da437aba4216fb8cfe5f0d197a4a5f
Rikstone <tarikbouari@gmail.com>
2022-09-02 11:05:54 -0700

    fix linters error

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,12 +1,12 @@
-import { getGame } from "./api.js";
-const gameCounter = document.getElementById("games");
+import { getGame } from './api.js';
 
-const countgame = async () => {
+const gameCounter = document.getElementById('games');
+
+export default async function gamecount () {
   const data = await getGame();
   const count = data.length;
   console.log(count);
   gameCounter.textContent = `Games(${count})`;
 };
-countgame();
 
-export { countgame };
+
