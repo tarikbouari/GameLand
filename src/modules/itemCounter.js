1ec1b57f7a229e52a7421ad74b11945ab36e5380
Rikstone <tarikbouari@gmail.com>
2022-09-02 09:33:37 -0700

    call game function

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,1 +1,11 @@
-g
+const gameCounter = document.getElementById("games");
+
+const countgame = async () => {
+  const data = await getGame();
+  const count = data.length;
+  console.log(count);
+  gameCounter.textContent = `Games(${count})`;
+};
+countgame();
+
+export { countgame };
